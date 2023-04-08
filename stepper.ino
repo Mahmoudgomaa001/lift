193ebb1c6a679e53ca58f997df174369b2fec3ba
Mahmoudgomaa001 <117765723+Mahmoudgomaa001@users.noreply.github.com>
2023-04-08 15:55:28 +0200

    u

---
+++ stepper.ino
@@ -0,0 +1,80 @@
+
+void stepper(double nbStep) {
+  if (nbStep >= 0) {
+    Direction = 1;
+  } else {
+    Direction = 0;
+    nbStep = -nbStep;
+  }
+  for (int x = 0; x < nbStep * 8; x++) {
+    switch (Steps) {
+      case 0:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        break;
+      case 1:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, HIGH);
+        break;
+      case 2:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+        break;
+      case 3:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+        break;
+      case 4:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+      case 5:
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+      case 6:
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+      case 7:
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        break;
+      default:
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+    }
+    delayMicroseconds(1000);
+    if (Direction == 1) {
+      Steps++;
+    }
+    if (Direction == 0) {
+      Steps--;
+    }
+    if (Steps > 7) {
+      Steps = 0;
+    }
+    if (Steps < 0) {
+      Steps = 7;
+    }
+  }
+}
